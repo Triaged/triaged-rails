247bb8ebae3c74e0fe6b9fcbd6afcdebe3717de3
Charlie White <charlie@anchor.io>
2013-09-27 08:17:25 -0700

    github refactor

---
+++ app/models/github/event/commit.rb
@@ -0,0 +1,15 @@
+class Github::Event::Commit
+  include Mongoid::Document
+
+  embedded_in :push, class_name: "Github::Event::Push"
+  
+  field :author, type: String
+  field :author_email, type: String
+  field :timestamp, type: DateTime
+  field :message, type: String
+  field :url, type: String
+  field :external_id, type: Integer
+
+  
+
+end
--- app/models/github/event/push.rb
+++ app/models/github/event/push.rb
@@ -5,39 +5,33 @@ class Github::Event::Push < FeedItem
   field :branch, :type => String
 
   belongs_to :repo, :class_name => "Github::Repo"
+  embeds_many :commits, :class_name => "Github::Event::Commit"
 
   
-  def commits
-  	repo.commits.where(:push_id => self.id)
-  end
-
-
   def self.build_from_webhook event
   	Rails.logger.info "------------"
+  	org = Github::Org.find_by name: event.repository.organization
+  	repo = org.repos.find_by name: event.repository.name
+
   	push = Github::Event::Push.new(
   		pusher: event.pusher.name,
   		branch: event.ref.split("/").last,
-  		external_id: event.head_commit.id
-  		)
+  		external_id: event.head_commit.id,
+  		repo: repo
+  	)
   	
-  	org = Github::Org.find_by name: event.repository.organization
-  	repo = org.repos.find_by name: event.repository.name
-  	# Embed the commits into the repo, since the repo holds the state.
-  	# Then, attach the commit to the push, to reference in the push event
-		event.commits.each do |commit|
+  	event.commits.each do |commit|
 			commit = commit.to_properties
-			repo.create(
+			push.commits.create(
   			external_id: commit.id,
   			author: commit.author.username,
   			author_email: commit.author.email,
   			timestamp: commit.timestamp,
   			message: commit.message,
   			url: commit.url,
-  			push: push
   			)
   	end
 
-  	push.repo = repo
   	return push
   end
 
