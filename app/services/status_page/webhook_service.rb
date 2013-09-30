4a4ee09911b0e9098d86203aef9a1eb97b402d57
Charlie White <charlie@anchor.io>
2013-09-30 11:31:24 -0700

    typo

--- app/services/status_page/webhook_service.rb
+++ app/services/status_page/webhook_service.rb
@@ -5,12 +5,12 @@ class Kiln::WebhookService < Service
 		service = payload[:service]
 		event_type = payload[:event_type]
 
-		publish(event_type, service, event})
+		publish(event_type, service, event)
   # rescue StandardError, e
   # 	Rails.logger.info e.inspect  
 	end
 
-	def publish(event_type, payload)
+	def publish(event_type, service, payload)
 		key =  "status_page.#{event_type}"
 		Rails.logger.info key
 		ActiveSupport::Notifications.instrument(key, payload)
