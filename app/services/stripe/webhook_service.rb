1816ee83849845d899107730a9f84e11b380b51e
Charlie White <charlie@anchor.io>
2013-10-02 18:08:35 -0700

    blowing up stripe customer, more, more

--- app/services/stripe/webhook_service.rb
+++ app/services/stripe/webhook_service.rb
@@ -2,8 +2,9 @@ class Stripe::WebhookService < Service
 	
 	def instrument payload
 		stripe_user = ProviderCredential.where(provider: Provider.named("stripe"), uid: payload[:user_id]).first.user
-		event = Stripe::Event.retrieve(payload[:id], stripe_user.stripe_provider_credentials.access_token)
-		event = retrieve_customer_details event
+		access_token = stripe_user.stripe_provider_credentials.access_token
+		event = Stripe::Event.retrieve(payload[:id], access_token)
+		event = retrieve_customer_details event, access_token
 		Rails.logger.info event.inspect
 		publish({:company_id => stripe_user.company.id, :event => event})
   # rescue StandardError, e
@@ -16,8 +17,8 @@ class Stripe::WebhookService < Service
 		Rails.logger.info "Published"
  	end
 
- 	def retrieve_customer_details event
- 		customer = Stripe::Customer.retrieve(event[:data][:object][:card][:customer])
+ 	def retrieve_customer_details event, access_token
+ 		customer = Stripe::Customer.retrieve(event[:data][:object][:card][:customer], access_token)
 		event[:customer_email] = customer[:email]
 		event[:customer_description] = customer[:description]
 		event
