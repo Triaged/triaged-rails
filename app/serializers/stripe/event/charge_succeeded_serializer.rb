6d739dc09fe80cea73ddb636fba99924ce8f26bb
Charlie White <charlie@anchor.io>
2013-10-02 18:14:03 -0700

    adding properties to stripe serializers

--- app/serializers/stripe/event/charge_succeeded_serializer.rb
+++ app/serializers/stripe/event/charge_succeeded_serializer.rb
@@ -1,5 +1,5 @@
 class	Stripe::Event::ChargeSucceededSerializer < EventSerializer
-	attributes :amount, :description
+	attributes :amount, :description, :customer_email, :customer_description
 
 	 
 end
