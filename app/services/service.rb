7ce7b2e11a89460d3d006aa2be85c09c26ddde98
Charlie White <charlie@anchor.io>
2013-10-07 11:27:08 -0700

    debugging github

--- app/services/service.rb
+++ app/services/service.rb
@@ -2,6 +2,7 @@ class Service
 
 	def publish(provider, event_type, payload)
 		event_name = "event.#{provider}.#{event_type}"
+		Rails.logger.info "Publishing #{event_name}"
 		ActiveSupport::Notifications.instrument(event_name, payload)
 		Rails.logger.info "Published #{event_name}"
  	end
