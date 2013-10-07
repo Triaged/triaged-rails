36727be31cd0f4be375d04556d5905933b635650
Charlie White <charlie@anchor.io>
2013-10-07 14:36:01 -0700

    min_id work

--- config/initializers/sentry.rb
+++
@@ -1,3 +0,0 @@
-ActiveSupport::Notifications.subscribe "sentry.exception" do |name, start, finish, id, payload|
-  Common::WebhookService.build_event_and_add_to_feeds(Sentry::Event::Exception, payload)
-end
