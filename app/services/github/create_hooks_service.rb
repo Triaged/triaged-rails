d707710dca86c7f47e06a348db8c0fef63156376
Charlie White <charlie@anchor.io>
2013-10-07 15:52:01 -0700

    fix for github webhooks, support all event types

--- app/services/github/create_hooks_service.rb
+++ app/services/github/create_hooks_service.rb
@@ -51,7 +51,7 @@ class Github::CreateHooksService
 			begin
 				@github.repos.hooks.create repo.owner, repo.name, name:  "web", active:  true, 
 					config: { "url" => services_github_index_url(subdomain: @company.slug)}, 
-					events: => ["push", "issues", "issue_comment", "pull_request", "commit_comment"]
+					events: ["push", "issues", "issue_comment", "pull_request", "commit_comment"]
 			rescue Github::Error::UnprocessableEntity
 				Rails.logger.info "hook already exists"
 				next
