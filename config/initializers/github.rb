23292ffb1b0cc8174e7d0c2724deed648700f47e
Charlie White <charlie@anchor.io>
2013-10-07 16:02:55 -0700

    adding some logging

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -7,10 +7,10 @@ ActiveSupport::Notifications.subscribe "event.github.push" do |name, start, fini
 end
 
 ActiveSupport::Notifications.subscribe "event.github.issues" do |name, start, finish, id, payload|
-	Rails.logger.info payload[:event][:event_action]
-	Common::WebhookService.build_event_and_add_to_feeds(Github::Event::IssueOpened, payload) if payload[:event][:event_action] == "opened"
-	Common::WebhookService.build_event_and_add_to_feeds(Github::Event::IssueReopened, payload) if payload[:event][:event_action] == "reopened"
-	Common::WebhookService.build_event_and_add_to_feeds(Github::Event::IssueClosed, payload) if payload[:event][:event_action] == "closed"
+	Rails.logger.info payload[:event][:action]
+	Common::WebhookService.build_event_and_add_to_feeds(Github::Event::IssueOpened, payload) if payload[:event][:action] == "opened"
+	Common::WebhookService.build_event_and_add_to_feeds(Github::Event::IssueReopened, payload) if payload[:event][:action] == "reopened"
+	Common::WebhookService.build_event_and_add_to_feeds(Github::Event::IssueClosed, payload) if payload[:event][:action] == "closed"
 end
 
 ActiveSupport::Notifications.subscribe "event.github.issue_comment" do |name, start, finish, id, payload|
