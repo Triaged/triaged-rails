ae5055d69a3d33e78e9bc871a64e84db21b64b26
Charlie White <charlie@anchor.io>
2013-10-06 20:16:06 -0700

    adding repo name to github push

--- app/serializers/github/event/push_serializer.rb
+++ app/serializers/github/event/push_serializer.rb
@@ -1,5 +1,9 @@
 class Github::Event::PushSerializer < FeedItemSerializer
-	attributes :pusher, :branch
+	attributes :pusher, :branch, :repo_name
 	has_many :commits
 	#has_one :repo
+
+	def repo_name
+		object.repo.name
+	end
 end
