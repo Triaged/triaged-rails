c939b7f241b68f18898b96cd402eb49154efbc64
Charlie White <charlie@anchor.io>
2013-10-09 11:05:33 -0700

    removed token spacing

--- app/models/push_token.rb
+++ app/models/push_token.rb
@@ -6,4 +6,10 @@ class PushToken
   field :service, type: String
   field :token, type: String
   field :count, type: Integer
+
+  before_create :strip_spaces
+
+  def strip_spaces
+  	token.delete(' ')
+  end
 end
