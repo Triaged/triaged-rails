01578e84f9702aeba0d68a38532afa4c32401d3e
Charlie White <charlie@anchor.io>
2013-10-10 12:49:38 -0700

    push token reset fix

--- app/controllers/api/v1/push_tokens_controller.rb
+++ app/controllers/api/v1/push_tokens_controller.rb
@@ -7,6 +7,7 @@ class Api::V1::PushTokensController < API::BaseController
 
 	def reset_count
 		push_tokens = current_user.push_tokens.where(service: push_params[:service])
+		Rails.logger.info current_user.inspect
 		push_tokens.each do |push_token| 
 			push_token.update_attribute(:count, 0)
 			Rails.logger.info push_token.inspect
