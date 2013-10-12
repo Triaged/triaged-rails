e5ab838324dee996f97b2114b6f2e7c16ae1f232
Charlie White <charlie@anchor.io>
2013-10-12 15:12:02 -0700

    turn stripe processing back on

--- app/services/stripe/webhook_service.rb
+++ app/services/stripe/webhook_service.rb
@@ -3,18 +3,18 @@ class Stripe::WebhookService < Service
 	@@provider = :stripe
 	
 	def instrument payload
-		# event_type = payload[:type]
-		# company = get_company payload
-		# access_token = company.stripe_provider_credentials.access_token
-		# Rails.logger.info company.inspect
+		event_type = payload[:type]
+		company = get_company payload
+		access_token = company.stripe_provider_credentials.access_token
+		Rails.logger.info company.inspect
 		
-		# # Validate event and fill customer data
-		# event = Stripe::Event.retrieve(payload[:id], access_token)
-		# payload = retrieve_customer_details payload, access_token
+		# Validate event and fill customer data
+		event = Stripe::Event.retrieve(payload[:id], access_token)
+		payload = retrieve_customer_details payload, access_token
 		
-		# publish(@@provider, event_type, {:company_id => company.id, :event => payload})
-  rescue StandardError, e
-  	Rails.logger.info e.inspect  
+		publish(@@provider, event_type, {:company_id => company.id, :event => payload})
+  # rescue StandardError, e
+  # 	Rails.logger.info e.inspect  
 	end
 
 	def get_company payload
