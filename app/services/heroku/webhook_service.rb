46d1b13a4c9fdb9cbe17fccbbb66fe3574a9e690
Charlie White <charlie@anchor.io>
2013-10-12 13:22:14 -0700

    new_relic wehbook service

--- app/services/heroku/webhook_service.rb
+++ app/services/heroku/webhook_service.rb
@@ -1,1 +1,15 @@
-class Heroku::WebhookService < Service end
+class Heroku::WebhookService < Service 
+
+	@@provider = :heroku
+	@@event_type = :release
+	
+	def instrument payload
+		publish(@@provider, @@event_type,  {
+			:company_id => payload[:company_id],
+			:event => payload[:event]
+		})
+  # rescue StandardError, e
+  # 	Rails.logger.info e.inspect  
+	end
+
+end
