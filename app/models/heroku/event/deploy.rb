a26e9d02ee3319ecb8f5f461ad5186e67a28d7d7
Charlie White <charlie@anchor.io>
2013-10-12 14:39:00 -0700

    Trying previous head...

--- app/models/heroku/event/deploy.rb
+++ app/models/heroku/event/deploy.rb
@@ -5,8 +5,9 @@ class Heroku::Event::Deploy < FeedItem
   field :user, :type => String
   field :git_log, :type => String
   field :head_long, :type => String
+  field :previous_head, :type => String
   field :head, :type => String
-  field :prev_head => String
+
 
   def self.build_from_webhook data
 		event = Heroku::Event::Deploy.new(
@@ -17,7 +18,7 @@ class Heroku::Event::Deploy < FeedItem
 			git_log: data.git_log,
 			head_long: data.head_long,
 			head: data.head,
-			prev_head: data.prev_head,
+			previous_head: data.prev_head,
 			timestamp: DateTime.now
 		)
 	end
