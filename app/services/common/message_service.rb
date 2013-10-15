9855631224e96cd68f16e9a80cd9af9bdceaa200
Charlie White <charlie@anchor.io>
2013-10-15 11:45:43 -0700

    messaging work

--- app/services/common/message_service.rb
+++ app/services/common/message_service.rb
@@ -2,7 +2,11 @@ module Common::MessageService
 
 	def self.new_message(feed_item, message_params)
 		company = feed_item.company
-		message = feed_item.messages.create(message_params)
+
+		
+		message = feed_item.messages.build(message_params)
+		#ensure feed_item.updated_at is tracked
+		feed_item.save!
 
 		company.followers_of(message.feed_item.provider).each do |follower|
 			Common::NotificationService.push_message follower, message
