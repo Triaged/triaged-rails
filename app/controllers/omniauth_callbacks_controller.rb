a79b95dc36297420a6b023039a04e2e5befa02ce
Charlie White <charlie@anchor.io>
2013-10-17 11:02:16 -0700

    fixing omniauth return

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -8,21 +8,20 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
 		params = request.env["omniauth.auth"]
-		logger.info params
-		user = current_user.save_omniauth("google_analytics", params['uid'], params['credentials']['token'], params['credentials']['refresh_token'])
+		result = current_user.save_omniauth("google_analytics", params['uid'], params['credentials']['token'], params['credentials']['refresh_token'])
     redirect_to oauth_complete_path
   end
 	
 
 	def github
 		params = request.env["omniauth.auth"]
-		user = current_user.save_omniauth("github", params['uid'], params['credentials']['token'])
-    redirect_to org_list_services_github(:subdomain => user.company)
+		result = current_user.save_omniauth("github", params['uid'], params['credentials']['token'])
+    redirect_to org_list_services_github(:subdomain => current_user.company)
   end
 
 	def stripe_connect
 		params = request.env["omniauth.auth"]
-    user = current_user.save_omniauth("stripe", params['uid'], params['credentials']['token'])
+    result = current_user.save_omniauth("stripe", params['uid'], params['credentials']['token'])
     redirect_to oauth_complete_path
   end
 
