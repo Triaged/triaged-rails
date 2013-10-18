ed84d5d936abb66425054142c9edc20b6ae610a1
Charlie White <charlie@anchor.io>
2013-10-17 21:46:07 -0700

    fixing ensure_connected_provider

--- config/initializers/common.rb
+++ config/initializers/common.rb
@@ -1,6 +1,6 @@
 ActiveSupport::Notifications.subscribe "provider_credentials.created" do |name, start, finish, id, payload|
 	company = Company.find(payload[:company_id])
-	provider = Provider.named(name.split(".").last)
+	provider = Provider.named(payload[:provider_name])
 
 	Common::ProviderConnection.ensure_connected(company, provider)
 end
