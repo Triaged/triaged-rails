16a926cfa3a756a3f041895260f4e601217315d8
Charlie White <charlie@anchor.io>
2013-10-20 16:24:45 -0700

    fix for airbrake wehbook

--- app/controllers/services/new_relic_controller.rb
+++ app/controllers/services/new_relic_controller.rb
@@ -1,6 +1,6 @@
 class Services::NewRelicController < ServiceController
 
-	def create
+	def webhook
 		company = Company.find(params[:id])
 		payload = {event: params, company_id: company.id, external_id: request.headers['X-Newrelic-Transaction']}
 		NewRelic::WebhookService.new.instrument(payload)
