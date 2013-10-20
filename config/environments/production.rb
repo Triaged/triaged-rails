27d3074b154f568e4ad4acab246f027f44792d27
Charlie White <charlie@anchor.io>
2013-10-20 14:50:16 -0700

    forcing ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ DockedRails::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -79,7 +79,7 @@ DockedRails::Application.configure do
       :password  => ENV["MANDRILL_API_KEY"]
     }
 
-  Rails.application.routes.default_url_options[:host] = "triaged.co"
+  Rails.application.routes.default_url_options[:host] = "www.triaged.co"
    
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
