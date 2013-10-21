edaf37e87bc06f688fd113678375279621bb311b
Charlie White <charlie@anchor.io>
2013-10-21 16:40:35 -0700

    fog region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      =>  ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key  =>  ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => 'eu-west-2'#,                  # optional, defaults to 'us-east-1'
+    :region                 => 'us-west-2'#,                  # optional, defaults to 'us-east-1'
     #:host                   => "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"             # optional, defaults to nil
   }
   config.fog_directory  = ENV['FOG_DIRECTORY']                             # required
