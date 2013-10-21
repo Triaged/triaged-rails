90fe1a5d5a1722b676fdc648f1e4d875cd3a19a2
Charlie White <charlie@anchor.io>
2013-10-21 16:35:48 -0700

    testing

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      =>  ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key  =>  ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => 'eu-west-2',                  # optional, defaults to 'us-east-1'
-    :host                   => "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"             # optional, defaults to nil
+    :region                 => 'eu-west-2'#,                  # optional, defaults to 'us-east-1'
+    #:host                   => "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"             # optional, defaults to nil
   }
   config.fog_directory  = ENV['FOG_DIRECTORY']                             # required
   #config.fog_public     = false                                   # optional, defaults to true
