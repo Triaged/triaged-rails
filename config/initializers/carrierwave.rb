eb07b563e9c0cf1e06f852368115e59fb18a8280
Charlie White <charlie@anchor.io>
2013-10-21 16:23:25 -0700

    still testing avatars..

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      =>  ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  =>  ENV['AWS_SECRET_ACCESS_KEY']
+    :aws_secret_access_key  =>  ENV['AWS_SECRET_ACCESS_KEY'],
     :region                 => 'eu-west-2',                  # optional, defaults to 'us-east-1'
     :host                   => "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"             # optional, defaults to nil
   }
