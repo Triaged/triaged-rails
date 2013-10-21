90cd934fc396eb02a1ebc5ffb0a33fe701fd8b5e
Charlie White <charlie@anchor.io>
2013-10-21 15:41:23 -0700

    testing to fix user

--- app/models/concerns/deviseable.rb
+++ app/models/concerns/deviseable.rb
@@ -13,9 +13,6 @@ module Deviseable
 	  field :email,              :type => String, :default => ""
 	  field :encrypted_password, :type => String, :default => ""
 
-	  validates_presence_of :email
-	  validates_presence_of :encrypted_password
-	  
 	  ## Recoverable
 	  field :reset_password_token,   :type => String
 	  field :reset_password_sent_at, :type => Time
@@ -48,9 +45,10 @@ module Deviseable
 	  # run 'rake db:mongoid:create_indexes' to create indexes
 	  index({ email: 1 }, { unique: true, background: true })
 	  field :name, :type => String
+	  
 	  validates_presence_of :name
-
-	  validates :email, :presence => true, :email => true
+	  validates_presence_of :encrypted_password
+		validates :email, :presence => true, :email => true
 	end
 
   def ensure_authentication_token!
--- app/models/user_feed_item.rb
+++ app/models/user_feed_item.rb
@@ -5,5 +5,5 @@ class UserFeedItem
   embedded_in :user
   belongs_to :feed_item
 
-  validates_uniqueness_of :feed_item
+  validates_uniqueness_of :feed_item_id
 end
