698cc1c53c95d6e8af53c12d0aa8a277e67f45c5
Charlie White <charlie@anchor.io>
2013-10-21 15:59:35 -0700

    s3 storage for carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,7 @@ CarrierWave.configure do |config|
     # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
  
     # Configuration for Amazon S3
+    :region                 => 'eu-west-2'
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
