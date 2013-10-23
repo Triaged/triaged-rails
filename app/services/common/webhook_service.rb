17f3b8eb2c3024edfc8a23538d409e00d96096be
Charlie White <charlie@anchor.io>
2013-10-22 22:13:05 -0700

    testing

--- app/services/common/webhook_service.rb
+++ app/services/common/webhook_service.rb
@@ -1,26 +1,11 @@
 class Common::WebhookService
-	 include Sidekiq::Worker
+
 
 	# Responsible for receiving webhook data, 
 	# packaging it into an internal event, 
 	# sending the event for feed processing
 
-	def perform event_class, payload
-		Rails.logger.info "Building event for #{event_class}"
-		company = Company.find(payload[:company_id])
-		event = event_class.build_from_webhook payload[:event].to_properties
-
-		# generic after init hook
-		event.after_build_hook company
-
-		# ensure the company knows this provider is connected
-		Common::ProviderConnection.ensure_connected(company, event.provider)
-	
-		# add event to company feed
-		Common::FeedService.add_to_feed event, company
-	end
-
 	def self.build_event_and_add_to_feeds event_class, payload
-		Common::WebhookService.perform_async(event_class, payload)
+		WebhookService.perform_async(event_class, payload)
 	end
 end
---
+++ app/workers/webhook_service.rb
@@ -0,0 +1,18 @@
+class WebhookService
+	include Sidekiq::Worker
+
+	def perform event_class, payload
+		Rails.logger.info "Building event for #{event_class}"
+		company = Company.find(payload[:company_id])
+		event = event_class.build_from_webhook payload[:event].to_properties
+
+		# generic after init hook
+		event.after_build_hook company
+
+		# ensure the company knows this provider is connected
+		Common::ProviderConnection.ensure_connected(company, event.provider)
+	
+		# add event to company feed
+		Common::FeedService.add_to_feed event, company
+	end
+end
