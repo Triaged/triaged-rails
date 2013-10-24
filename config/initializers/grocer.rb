4e8678f09c7fadd796b58767a41703a6900de83f
Charlie White <charlie@anchor.io>
2013-10-24 15:59:57 -0700

    production push creds

--- config/initializers/grocer.rb
+++ config/initializers/grocer.rb
@@ -1,5 +1,5 @@
 GROCER = Grocer.pusher(
-  certificate: Rails.root + "docked-dev-apns.pem",      # required
+  certificate: Rails.root + "triage-apns.pem",      # required
   passphrase:  "snowboard12",                       # optional
   gateway:     "gateway.sandbox.push.apple.com", # optional; See note below.
   port:        2195,                     # optional
