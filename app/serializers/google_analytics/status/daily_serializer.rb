dcca6b78270e2c432edb8a7a431b36baf784498f
Charlie White <charlie@anchor.io>
2013-10-27 16:19:54 -0700

    testing

--- app/serializers/google_analytics/status/daily_serializer.rb
+++ app/serializers/google_analytics/status/daily_serializer.rb
@@ -2,4 +2,8 @@ class GoogleAnalytics::Status::DailySerializer < GraphItemSerializer
   #attributes :date, :visits_sum, :visitors_sum, :pageviews_sum
 
   #has_many :daily_details
+
+  def property
+  	"@todo"
+  end
 end
