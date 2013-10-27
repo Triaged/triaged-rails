dc68d5630ebc8de295eb57d1faba8cc73807d3bc
Charlie White <charlie@anchor.io>
2013-10-27 15:56:58 -0700

    small test

--- app/serializers/feed_item_serializer.rb
+++ app/serializers/feed_item_serializer.rb
@@ -2,9 +2,7 @@ class FeedItemSerializer < ActiveModel::Serializer
   attribute :provider_name, key: :provider
 	attribute :event_name, key: :event
   attributes :id, :timestamp, :html_url, :updated_at
-  attribute :property
-
-
+  
   has_many :messages
 
   def messages_count
