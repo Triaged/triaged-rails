1b4e334dca0f53305cb3b4a2f8cb74b01e271fd2
Charlie White <charlie@anchor.io>
2013-10-27 16:17:12 -0700

    more testing

--- app/serializers/heroku/event/deploy_serializer.rb
+++ app/serializers/heroku/event/deploy_serializer.rb
@@ -1,3 +1,11 @@
-class Heroku::Event::DeploySerializer < FeedItemSerializer
-	attributes :app, :user, :git_log, :head_long, :head, :previous_head
+class Heroku::Event::DeploySerializer < TextItemSerializer
+	#attributes :app, :user, :git_log, :head_long, :head, :previous_head
+
+	def property
+		object.app
+	end
+
+	def body
+		object.git_log
+	end
 end
