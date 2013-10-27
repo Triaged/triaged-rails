802567d0af17617ebf97fe0642aa544eec06d850
Charlie White <charlie@anchor.io>
2013-10-27 15:54:25 -0700

    small test

--- app/serializers/feed_item_serializer.rb
+++ app/serializers/feed_item_serializer.rb
@@ -2,7 +2,6 @@ class FeedItemSerializer < ActiveModel::Serializer
   attribute :provider_name, key: :provider
 	attribute :event_name, key: :event
   attributes :id, :timestamp, :html_url, :updated_at
-  attribute :card_type
   attribute :property
 
 
