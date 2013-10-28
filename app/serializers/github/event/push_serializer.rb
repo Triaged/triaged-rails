2a665d92c0d412b54c88fb240c92770db0c34df6
Charlie White <charlie@anchor.io>
2013-10-28 08:49:48 -0700

    formatting

--- app/serializers/github/event/push_serializer.rb
+++ app/serializers/github/event/push_serializer.rb
@@ -9,11 +9,11 @@ class Github::Event::PushSerializer < TextItemSerializer
 	end
 
 	def action
-		"#{object.pusher.humanize} pushed to #{object.branch}"
+		"#{object.pusher.underscore.humanize} pushed to #{object.branch}"
 	end
 
 	def body
-		 object.commits.collect {|commit| commit.message }.join("\n")
+		 object.commits.collect {|commit| commit.message.capitalize }.join("\n")
 	end
 
 end
--- app/serializers/kiln/event/push_serializer.rb
+++ app/serializers/kiln/event/push_serializer.rb
@@ -7,10 +7,10 @@ class Kiln::Event::PushSerializer < TextItemSerializer
 	end
 
 	def action
-		"#{object.pusher.humanize} pushed to #{object.branch}"
+		"#{object.pusher.underscore.humanize} pushed to #{object.branch}"
 	end
 
 	def body
-		object.commits.collect {|commit| commit.message }.join("\n")
+		object.commits.collect {|commit| commit.message.capitalize }.join("\n")
 	end
 end
