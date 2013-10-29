47e20630294e3df9ed788d08a9374878ac315df9
Charlie White <charlie@anchor.io>
2013-10-29 09:34:42 -0700

    sentry html_url

--- app/models/sentry/event/exception.rb
+++ app/models/sentry/event/exception.rb
@@ -21,7 +21,7 @@ class Sentry::Event::Exception < FeedItem
 	end
 
 	def build_html_url
-		self.html_url = "https://app.getsentry.com/"
+		self.html_url = "https://app.getsentry.com/#{project}/group/#{external_id}"
 	end
 
 	def should_push?
