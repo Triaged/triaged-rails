798066cfddd2a658a93b74e053dd13c4697e021f
Charlie White <charlie@anchor.io>
2013-10-29 10:00:29 -0700

    testing ga

--- app/serializers/google_analytics/status/daily_serializer.rb
+++ app/serializers/google_analytics/status/daily_serializer.rb
@@ -6,6 +6,6 @@ class GoogleAnalytics::Status::DailySerializer < GraphItemSerializer
 
   def action
   	Rails.logger.info object.date.to_s
-  	Date.strptime(object.date.to_s, '%A, %b %d')
+  	object.date.strftime('%A, %b %d')
   end
 end
