d78637115a0e8c8d410771bc4db79f97d2c0f2e6
Charlie White <charlie@anchor.io>
2013-10-30 13:24:18 -0700

    more work

--- app/models/google_analytics/status/daily.rb
+++ app/models/google_analytics/status/daily.rb
@@ -15,7 +15,8 @@ class GoogleAnalytics::Status::Daily < FeedItem
 		self.html_url = "http://www.google.com/analytics/"
 	end
 
-	def ga_property
-		company.default_google_analytics_account.properties.where(external_id: self.property_external_id).first
+	def ga_property_name
+		ga_property = company.default_google_analytics_account.properties.where(external_id: self.property_external_id).first
+		ga_property ? ga_property.name : nil
 	end
 end
--- app/serializers/google_analytics/status/daily_serializer.rb
+++ app/serializers/google_analytics/status/daily_serializer.rb
@@ -1,7 +1,7 @@
 class GoogleAnalytics::Status::DailySerializer < GraphItemSerializer
   
 	def property
-  	object.ga_property.name
+  	object.ga_property_name
   end
 
   def action
