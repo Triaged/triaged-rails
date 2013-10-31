d7986bce260cc7315375138662fac3e8bd7c54f0
Charlie White <charlie@anchor.io>
2013-10-30 20:41:40 -0700

    trying sandbox

--- config/initializers/grocer.rb
+++ config/initializers/grocer.rb
@@ -1,13 +1,13 @@
 GROCER = Grocer.pusher(
-  certificate: Rails.root + "triage-prod-apns.pem",      # required
+  certificate: Rails.root + "triage-apns.pem",      # required
   passphrase:  "snowboard12",                       # optional
-  gateway:     "gateway.push.apple.com", # optional; See note below.
+  gateway:     "gateway.sandbox.push.apple.com", # optional; See note below.
   port:        2195,                     # optional
   retries:     3                         # optional
 )
 
 GROCER_FEEDBACK = Grocer.feedback(
-  certificate: Rails.root + "triage-prod-apns.pem",       # required
+  certificate: Rails.root + "triage-apns.pem",       # required
   passphrase:  "snowboard12",                        # optional
   gateway:     "feedback.push.apple.com", # optional; See note below.
   port:        2196,                      # optional
