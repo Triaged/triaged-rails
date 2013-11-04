5fd9822b1d1b38e7866fede506c7f15542305f72
Charlie White <charlie@anchor.io>
2013-11-04 13:03:55 -0800

    commit formatter

--- app/serializers/kiln/event/push_serializer.rb
+++ app/serializers/kiln/event/push_serializer.rb
@@ -15,7 +15,8 @@ class Kiln::Event::PushSerializer < TextItemSerializer
 	end
 
 	def commits_formatter commits
-		body = commits.take(5).collect {|commit| commit.message.capitalize }.join("\n")
-		body += "\nAnd #{commits.count - 5} more" if commits.count > 5
+		body = commits.take(5).collect {|commit| "- #{commit.message.capitalize}" }.join("\n")
+		body += "\nand #{commits.count - 5} more" if commits.count > 5
+		return body
 	end
 end
