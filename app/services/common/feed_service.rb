d0895961306667ea76d50df9e0495b0d68e7cf55
Charlie White <charlie@anchor.io>
2013-11-06 21:41:40 -0800

    more feed service refactoring

--- app/services/common/feed_service.rb
+++ app/services/common/feed_service.rb
@@ -1,9 +1,13 @@
 module Common::FeedService
 
 	def self.add_to_feed(event, company)
-		Rails.logger.info "Adding event #{event.provider_name}:#{event.event_name} to company feed: #{company.name}"
-		event = company.add_event_to_feed event if event
-		true
+		if event
+			Rails.logger.info "Adding event #{event.provider_name}:#{event.event_name} to company feed: #{company.name}"
+			event = company.add_event_to_feed event 
+			return true
+		else
+			return false
+		end
 	end 
 
 end
