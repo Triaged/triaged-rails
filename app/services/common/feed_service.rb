9a784fe120d77f93b51312f21ca368962361bade
Charlie White <charlie@anchor.io>
2013-11-06 21:23:37 -0800

    protect against nil events

--- app/services/common/feed_service.rb
+++ app/services/common/feed_service.rb
@@ -2,7 +2,7 @@ module Common::FeedService
 
 	def self.add_to_feed(event, company)
 		Rails.logger.info "Adding event to feed"
-		event = company.add_event_to_feed event
+		event = company.add_event_to_feed event if event
 		true
 	end 
 
