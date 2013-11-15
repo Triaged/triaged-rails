314c319220e7264279567804fce806172e19d904
Charlie White <charlie@anchor.io>
2013-11-15 10:19:38 -0800

    adding token to custom url scheme

--- app/controllers/verify_controller.rb
+++ app/controllers/verify_controller.rb
@@ -4,6 +4,7 @@ class VerifyController < ApplicationController
 		@user = User.where(id: params[:id], company_validation_token: params[:token]).first
 		logger.info @user.inspect
 		if @user
+			@token =  params[:token]
 			@user.update_attribute(:validated_belongs_to_company, true)
 		else
 			redirect_to(root_path) and return
