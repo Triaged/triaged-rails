d5c76c41475a886532b9aa4c417182ff69242043
Charlie White <charlie@anchor.io>
2013-11-14 16:01:21 -0800

    testing company_id to string

--- app/services/common/webhook_service.rb
+++ app/services/common/webhook_service.rb
@@ -7,6 +7,10 @@ class Common::WebhookService
 
 	def self.build_event_and_add_to_feeds event_class, payload
 		# Resque.enqueue(WebhookService, event_class, payload)
-		 AsyncWebhookService.perform_async(event_class, payload)
+
+		# Hack to fix dump moped id
+		payload[:company_id] = payload[:company_id].to_s
+		
+		AsyncWebhookService.perform_async(event_class, payload)
 	end
 end
