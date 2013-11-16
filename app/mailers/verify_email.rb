5fb1852d47c8840f372a5b4cb4e8e10a4d1cdde2
Charlie White <charlie@anchor.io>
2013-11-16 10:18:34 -0800

    verify email

--- app/mailers/verify_email.rb
+++ app/mailers/verify_email.rb
@@ -7,12 +7,13 @@ def initialize(user_id)
 	end
 
 	def template_content
+		Rails.logger.info verify_email_url(:id => @user.id, :token => @user.company_validation_token)
 		[{
-			:name => 'fname',
+			:name => 'FNAME',
 			:content => @user.name.split.first
 		},
 		{
-			:name => 'vlink',
+			:name => 'VLINK',
 			:content => verify_email_url(:id => @user.id, :token => @user.company_validation_token)
 		}]
 	end
