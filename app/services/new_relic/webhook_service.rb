e647974d93bedba4743db682a55e7d834d01f33b
Charlie White <charlie@anchor.io>
2013-11-18 15:02:01 -0800

    fixing new_relic typo

--- app/services/new_relic/webhook_service.rb
+++ app/services/new_relic/webhook_service.rb
@@ -20,8 +20,8 @@ class NewRelic::WebhookService < Service
 
  	def alert_type alert
  		alert = JSON.parse(alert)
- 		return "downtime" if (alert["severity"] == "downtime" && alert["short_descripton"].include?("opened"))
-		return "downtime_ended" if (alert["severity"] == "downtime" && alert["short_descripton"].include?("ended"))
+ 		return "downtime" if (alert["severity"] == "downtime" && alert["short_description"].include?("opened"))
+		return "downtime_ended" if (alert["severity"] == "downtime" && alert["short_description"].include?("ended"))
 		return "error_threshold" if (alert["message"].include?("Error rate") && alert["short_description"].include?("opened"))
 		return "error_threshold_ended" if (alert["message"].include?("Error rate") && alert["short_description"].include?("ended"))
 		return "apdex_alert" if (alert["message"].include?("Apdex score") && alert["short_description"].include?("opened"))
