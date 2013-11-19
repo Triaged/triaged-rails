566e533d6c8254950a58abeb2dd06fa4d5919e24
Charlie White <charlie@anchor.io>
2013-11-18 18:25:23 -0800

    tweaking stripe serializer

--- app/serializers/stripe/event/charge_refunded_serializer.rb
+++ app/serializers/stripe/event/charge_refunded_serializer.rb
@@ -2,14 +2,14 @@ class Stripe::Event::ChargeRefundedSerializer < TextItemSerializer
   #attributes :amount, :description
 
   def property
-		"Live"
+		""
 	end
 
 	def action
-		"Charge Refunded"
+		"$#{object.amount} Charge Refunded"
 	end
 
 	def body
-		"#{object.amount}\n#{object.description}"
+		object.description
 	end
 end
--- app/serializers/stripe/event/charge_succeeded_serializer.rb
+++ app/serializers/stripe/event/charge_succeeded_serializer.rb
@@ -2,11 +2,11 @@ class	Stripe::Event::ChargeSucceededSerializer < TextItemSerializer
 	#attributes :amount, :description, :customer_email, :customer_name
 
 	def property
-		"Live"
+		""
 	end
 
 	def action
-		"#{object.amount}$ Charge Succeeded"
+		"$#{object.amount} Charge Succeeded"
 	end
 
 	def body
