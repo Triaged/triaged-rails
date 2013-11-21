d4358643d60522f6a76d950f792c7f64ce23e4b1
Charlie White <charlie@anchor.io>
2013-11-20 22:21:46 -0800

    admin touchup

--- app/controllers/admin/welcome_controller.rb
+++ app/controllers/admin/welcome_controller.rb
@@ -5,6 +5,7 @@ class Admin::WelcomeController < ApplicationController
 		@user_count = User.count
 		@company_count = Company.count
 		@feed_item_count = Company.all.sum {|company| company.feed_items.count}
+		@connected_count = Company.all.sum {|company| company.connected_providers.count}
 	end
 
 end
