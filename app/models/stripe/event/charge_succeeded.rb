33e34cf5562d50f3b50ed65fd8cbc97c0090d9ac
Charlie White <charlie@anchor.io>
2013-11-21 20:59:47 -0800

    stripe fixes

--- app/models/stripe/event/charge_succeeded.rb
+++ app/models/stripe/event/charge_succeeded.rb
@@ -2,10 +2,13 @@ class Stripe::Event::ChargeSucceeded < Stripe::BaseEvent
   include Mongoid::Document
 
 	def self.build_from_webhook data
+
+		description = data.data.object.description.nil? ? nil : data.data.object.description,
+
   	event = Stripe::Event::ChargeSucceeded.new(
 			amount: (data.data.object.amount / 100),
 			customer_id: data.data.object.customer,
-			description: data.data.object.description unless data.data.object.description.nil?,
+			description: description,
 			object_id: data.data.object.id,
 			external_id: data.id,
 			timestamp: DateTime.strptime(data.created.to_s,'%s')
