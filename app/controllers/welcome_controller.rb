2b21178b2dc30a627b82f1dfcb6249ce63ba4fb4
Charlie White <charlie@anchor.io>
2013-12-04 18:33:43 -0800

    adding text me link

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -19,10 +19,10 @@ class WelcomeController < ApplicationController
 	  @response =  "Great, we will be in touch shortly!"
 	end
 
-	# def deliver_sms
-	# 	sms_app_link = Common::SmsService.new(params[:sms_delivery][:phone_number])
- #    result = sms_app_link.deliver!
+	def deliver_sms
+		sms_app_link = Common::SmsService.new(params[:sms_delivery][:phone_number])
+    result = sms_app_link.deliver!
     
- #    @response =  result ? "Great, we sent you a text message!" : "Sorry, your phone number looks invalid."
-	# end
+    @response =  result ? "Great, we sent you a text message!" : "Sorry, your phone number looks invalid."
+	end
 end
