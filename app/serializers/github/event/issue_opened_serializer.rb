16c0a06c4aadd06cfebe45f0601c5b02101abf6a
Charlie White <charlie@anchor.io>
2013-12-13 10:50:39 -0800

    serializer

--- app/serializers/github/event/issue_opened_serializer.rb
+++ app/serializers/github/event/issue_opened_serializer.rb
@@ -10,11 +10,9 @@ class Github::Event::IssueOpenedSerializer < TextItemSerializer
 	end
 
 	def body
-		Rails.logger.info "BODY----"
-		Rails.logger.info object.body_text.nil?
-		body = object.body_text.nil? ? "---" : object.body_text 
-		Rails.logger.info body
-		body += "\n\nAssigned to #{object.assigned_to_name}" if object.assigned_to_name
+		body = object.body_text.nil? ? " " : object.body_text 
+		body += "\n\n" if (object.body_text && object.assigned_to_name)
+		body += "Assigned to #{object.assigned_to_name}" if object.assigned_to_name
 		return body
 	end
 end
