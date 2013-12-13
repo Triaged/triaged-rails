4be94c609edc12cc8f7b25e4340b33d730c08c5d
Charlie White <charlie@anchor.io>
2013-12-12 21:20:33 -0800

    beanstalk fix

--- app/controllers/services/beanstalk_controller.rb
+++ app/controllers/services/beanstalk_controller.rb
@@ -2,7 +2,7 @@ class Services::BeanstalkController < ServiceController
 
 	def webhook
 		event_type = "push"
-		payload = {event: params, company_id: params[:company_id], event_type: event_type}
+		payload = {event: params["payload"], company_id: params[:company_id], event_type: event_type}
 		Beanstalk::WebhookService.new.instrument payload
 		head :ok
 	# rescue StandardError
