d6263cc1f9391daf7ebda1b85ee50b2d5fb39b70
Charlie White <charlie@anchor.io>
2013-12-12 20:41:56 -0800

    crashlytics initializer

--- config/initializers/crashlytics.rb
+++ config/initializers/crashlytics.rb
@@ -1,1 +1,3 @@
-# Add initialization content here
+ActiveSupport::Notifications.subscribe "event.crashlytics.issue" do |name, start, finish, id, payload|
+  Common::WebhookService.build_event_and_add_to_feeds(Crashlytics::Event::Issue, payload)
+end
