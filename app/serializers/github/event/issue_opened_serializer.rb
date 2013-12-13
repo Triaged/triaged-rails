cda8503426127e8feb9d4d38d2c8e417e2ab00e8
Charlie White <charlie@anchor.io>
2013-12-13 11:14:15 -0800

    serializer

--- app/serializers/github/event/issue_opened_serializer.rb
+++ app/serializers/github/event/issue_opened_serializer.rb
@@ -6,11 +6,12 @@ class Github::Event::IssueOpenedSerializer < TextItemSerializer
 	end
 
 	def action
-		object.title
+		"Issue Opened"
 	end
 
 	def body
-		body = object.body_text.nil? ? " " : object.body_text 
+		body = object.title
+		body +=  "\n\n #{object.body_text}" unless object.body_text.nil?
 		body += "\n\n" if (!object.body_text.nil? && object.assigned_to_name)
 		body += "Assigned to #{object.assigned_to_name}" if object.assigned_to_name
 		return body
