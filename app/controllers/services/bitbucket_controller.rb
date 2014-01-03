af9b459ac4053492ae0a19221ee58d3baede3c4e
Charlie White <charlie@triaged.co>
2014-01-03 14:44:44 -0800

    fixing bitbucket

--- app/controllers/services/bitbucket_controller.rb
+++ app/controllers/services/bitbucket_controller.rb
@@ -4,7 +4,7 @@ class Services::BitbucketController < ServiceController
 
 	def webhook
 		event = JSON.parse(params["payload"])
-		payload = {event: event, company_id: params[:company_id], event_type: event_type}
+		payload = {event: event, company_id: params[:company_id]}
 		Bitbucket::WebhookService.new.instrument payload
 		head :ok
 	# rescue StandardError
