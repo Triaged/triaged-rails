0582e67500de6d10f309447e7270342a95226611
Charlie White <charlie@triaged.co>
2014-01-03 15:36:17 -0800

    verifying crashlytics

--- app/controllers/services/crashlytics_controller.rb
+++ app/controllers/services/crashlytics_controller.rb
@@ -2,6 +2,12 @@ class Services::CrashlyticsController < ServiceController
 	respond_to :json
 
 	def webhook
+		event_type = params[:event]
+
+		if (event_type == "verification")
+			Company.find(params[:company_id])
+		end
+
 		payload = {event: params[:payload], company_id: params[:company_id]}
 		Crashlytics::WebhookService.new.instrument(payload)
 		head :ok
