bd41746852edccc4fc1e674ab7a2db03b1cb4425
Charlie White <charlie@triaged.co>
2014-01-09 14:53:53 -0800

    services

--- app/workers/async_webhook_service.rb
+++ app/workers/async_webhook_service.rb
@@ -31,7 +31,7 @@ class AsyncWebhookService
 			event.timestamp = payload[:timestamp]
 
 			# ensure the company knows this provider is connected
-			Common::ProviderConnection.ensure_connected(company, event.provider_from_name)
+			Common::ProviderConnection.ensure_connected(company, event.provider)
 		
 			# add event to company feed
 			Common::FeedService.add_to_feed event, company
