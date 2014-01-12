649c62e0bb917972a779ea4c1df8fa6f1a19ea0c
Charlie White <charlie@triaged.co>
2014-01-12 13:11:33 -0800

    event sets

--- app/controllers/app/feed_controller.rb
+++ app/controllers/app/feed_controller.rb
@@ -3,7 +3,7 @@ class App::FeedController < ApplicationController
 	respond_to :html
 
 	def index
-		@feed_items = FeedItem.all
+		@feed_items = current_user.feed(params[:min_updated_at], params[:max_updated_at])
 		respond_with @feed
 	end
 end
