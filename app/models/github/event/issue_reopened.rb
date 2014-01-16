ee87fdec63ac5a2145c089585828b8d3045ab73d
Charlie White <charlie@triaged.co>
2014-01-16 11:52:22 -0800

    issue reopened

--- app/models/github/event/issue_reopened.rb
+++ app/models/github/event/issue_reopened.rb
@@ -8,20 +8,21 @@ class Github::Event::IssueReopened < FeedItem
   field :body, type: String
   
   def self.build_from_webhook event
-  	assigned_to_name = (event.issue.assignee != nil) ? event.issue.assignee.login : nil
-  	open = event.issue.state == "open" ? true : false
+    assigned_to_name = event.issue.respond_to?(:assignee) && event.issue.assignee ?  event.issue.assignee.login : nil
+    org_name = event.repository.respond_to?(:owner) ? event.repository.owner.login : event.user.login
+    open = event.issue.state == "open" ? true : false
 
-  	issue_opened_event = Github::Event::IssueOpened.new(
-  		title: event.issue.title,
-  		opened_by_name: event.issue.user.login,
-  		assigned_to_name: assigned_to_name,
-  		body: event.issue.body,
-  		open: open,
-  		html_url: event.issue.html_url,
-  		external_id: event.issue.id,
-  		org_name: event.repository.owner.login,
-  		repo_name: event.repository.name
-  	)
-  	return issue_opened_event
-	end
+    issue_opened_event = Github::Event::IssueReopened.new(
+      title: event.issue.title,
+      opened_by_name: event.issue.user.login,
+      assigned_to_name: assigned_to_name,
+      body_text: event.issue.body,
+      open: open,
+      html_url: event.issue.html_url,
+      external_id: event.number,
+      org_name: org_name,
+      repo_name: event.repository.name
+    )
+    return issue_opened_event
+  end
 end
--- app/serializers/github/event/issue_reopened_serializer.rb
+++ app/serializers/github/event/issue_reopened_serializer.rb
@@ -1,14 +1,18 @@
 class Github::Event::IssueReopenedSerializer < TextItemSerializer
   
   def property
-		"@todo"
+		object.repo_name.capitalize
 	end
 
 	def action
-		"@todo"
+		"Issue Reopened"
 	end
 
 	def body
-		"@todo"
+		body = object.title
+		body +=  "\n#{object.body_text}" unless object.body_text.nil?
+		body += "\n\n" if (!object.body_text.nil? && object.assigned_to_name)
+		body += "Assigned to #{object.assigned_to_name}" if object.assigned_to_name
+		return body
 	end
 end
