25010858dfa816d1451be2126e34dd763a4f94ae
Charlie White <charlie@triaged.co>
2014-01-30 08:37:08 -0800

    fixing github

--- app/models/github/base_event.rb
+++ app/models/github/base_event.rb
@@ -5,19 +5,20 @@ class Github::BaseEvent < FeedItem
 
   belongs_to :provider_account
 
-  def repo
-  	provider_account.provider_properties.find_by name: repo_name
-  end
+  # def repo
+  # 	provider_account.provider_properties.find_by(name: repo_name)
+  # end
 
 	def after_build_hook company
 		provider_account = company.provider_accounts.find_by(provider: Provider.named("github"), name: org_name, default: true)
 		Rails.logger.info "Provider: #{provider_account}"
 		self.provider_account = provider_account
+	rescue
 	end
 
-	def ignorable_objects
-		[repo]
-	end
+	# def ignorable_objects
+	# 	[repo]
+	# end
 
 end
 
