1688e064bafb5707d300373c3071b9170d86400f
Charlie White <charlie@triaged.co>
2014-01-30 09:05:57 -0800

    braintree

--- app/controllers/services/braintree_controller.rb
+++ app/controllers/services/braintree_controller.rb
@@ -1,5 +1,10 @@
 class Services::BraintreeController < ServiceController 
 
+	def show
+		@response = Braintree::WebhookNotification.verify(params[:bt_challenge])
+		respond_with @response
+	end
+
 	# Can't use subdomains for stripe
 	def webhook
 		payload = {event: params, company_id: params[:company_id]}
