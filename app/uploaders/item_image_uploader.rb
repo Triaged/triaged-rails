c91ea8c4962400da3617c0343b6a4c9a6841445d
Charlie White <charlie@triaged.co>
2014-01-31 10:53:17 -0800

    event view

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -7,7 +7,7 @@ class ItemImageUploader < CarrierWave::Uploader::Base
  
   version :ios do
     #eager
-    cloudinary_transformation :width => 600, :crop => :limit
+    cloudinary_transformation [{:width => 600, :crop => :limit}, { :width => 0.5, :crop => :scale }]
   end
   
 
