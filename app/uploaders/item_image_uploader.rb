1be805a48b0cad04a78362542ccb144197174598
Charlie White <charlie@triaged.co>
2014-01-31 11:18:39 -0800

    fixing transformation

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -4,6 +4,11 @@ class ItemImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
  
+  version :web do
+    #eager
+    cloudinary_transformation :transformation => [{:width => 600, :crop => :limit}]
+  end
+
   version :ios do
     #eager
     cloudinary_transformation :transformation => [{:width => 600, :crop => :limit}, { :width => 0.5, :crop => :scale }]
