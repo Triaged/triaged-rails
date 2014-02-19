33c0a94da05fcf942212f8d088e19bc1aca2c6e4
Charlie White <charlie@triaged.co>
2014-02-19 13:29:33 -0800

    removed html_url

--- app/serializers/feed_item_serializer.rb
+++ app/serializers/feed_item_serializer.rb
@@ -4,9 +4,26 @@ class FeedItemSerializer < ActiveModel::Serializer
  
   attributes :id, :timestamp, :updated_at
   has_many :messages
+  has_one :provider
+  attributes :event_name
+  attributes :type, :external_id, :timestamp, :title, :body, :footer, :url, :property_name, :image_url, :thumbnail_url
 
   def messages_count
   	object.messages.count
+  end
+
+
+
+  def type
+    :event
+  end
+
+  def body
+    object.body || object.body_list.collect {|entry| "- #{entry}" }.join("\n")
+  end
+
+  def image_url
+    object.event_image.ios.url
   end
 
   def list_helper list
