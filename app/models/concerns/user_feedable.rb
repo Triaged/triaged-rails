77822b7d22771d8a2d7597739b665a283b576913
Charlie White <charlie@triaged.co>
2014-02-19 11:36:31 -0800

    feed retrieval

--- app/models/concerns/user_feedable.rb
+++ app/models/concerns/user_feedable.rb
@@ -15,7 +15,7 @@ module UserFeedable
 
   # 	FeedItem.desc(:created_at).find(feed_items.collect {|item| item.feed_item_id })
 
-    feed_items.limit(100).order(:desc)
+  feed_items.limit(100).order(created_at: :desc)
   	#company.feed_items.desc(:created_at).find(feed_items.collect {|item| item.feed_item_id })
   end
 
