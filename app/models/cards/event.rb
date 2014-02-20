4bd77a1b2b6e13257239042f1f8ffa357179913f
Charlie White <charlie@triaged.co>
2014-02-20 12:33:06 -0800

    after_build_hook

--- app/models/cards/event.rb
+++ app/models/cards/event.rb
@@ -4,11 +4,11 @@ class Cards::Event < FeedItem
 	#validates :body, presence: true, :unless => :body_list
 	#validates :body_list, presence: true, :unless => :body
 
-	def after_build_hook company, payload
+	def after_build_hook company
 		super
 		
 		# Set timestamp if we don't already have one
-		self.timestamp = payload[:timestamp] unless self.timestamp
+		self.timestamp = DateTime.now unless self.timestamp
 
 		# Set Provider name
 		self.provider_name = self.provider.name
