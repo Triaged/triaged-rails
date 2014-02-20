4e333495f8cfc4bcda22d7e2ef2dbd044b1ceb9d
Charlie White <charlie@triaged.co>
2014-02-20 12:29:24 -0800

    dropbox updates

--- app/services/dropbox/event/update.rb
+++ app/services/dropbox/event/update.rb
@@ -1,33 +1,31 @@
 class Dropbox::Event::Update < BaseServiceEvent
-	
-	def self.build_from_delta result
+  
+  def self.build_from_delta path, metadata, company
+    #puts path
+    status =  (metadata != nil) ? :updated : :deleted
 
-		event_set = {
-      type: :event_set,
+    access_token = company.dropbox_provider_credentials.access_token
+    image_url = (metadata && metadata['thumb_exists']) ? "https://api-content.dropbox.com/1/thumbnails/dropbox#{path}?size=xl&access_token=#{access_token}" : nil
+
+    event = {
       company_id: company.id.to_s,
-      provider: {name: self.provider_name }, event_name: self.event_name,
+      provider: {name: self.provider_name }, 
+      event_name: self.event_name,
+      account_name: nil,
+      property_name: nil,
+      external_id: metadata['rev'],
       title: "Files Uploaded",
-      timestamp: DateTime.now,
-      push_notify: false
+      body: "#{path} #{status}",
+      image_url: image_url,
+      footer: nil,
+      url: nil,
+      timestamp: metadata['modified'],
+      push_notify: false,
+      group_event: false,
+
     }
-
-    event_set[:events] = []
-    for path, metadata in result['entries']
-			#puts path
-			status =  (metadata != nil) ? :updated : :deleted
-      event_set[:events] << {
-        external_id: metadata['rev'],
-        property_name: data.repository.name,
-        body: "#{path} #{status}",
-        footer: commit.branch,
-        timestamp: metadata['modified'],
-        url: commit.url,
-        thumbnail_url: metadata['thumb_exists'],
-      }
-    end
-
-    return event_set.to_json
-	end
+    return event.to_json
+  end
 
 
 end
