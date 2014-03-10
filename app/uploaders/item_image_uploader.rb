d5d21ab8a744a555833e9953746addf8527defc4
Charlie White <charlie@triaged.co>
2014-03-10 10:15:55 -0700

    full size images

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -11,7 +11,7 @@ class ItemImageUploader < CarrierWave::Uploader::Base
 
   version :ios do
     #eager
-    cloudinary_transformation :transformation => [{:width => 600, :crop => :limit}, { :width => 0.5, :crop => :scale }]
+    cloudinary_transformation :transformation => [{:width => 600, :crop => :limit}]
   end
   
 
