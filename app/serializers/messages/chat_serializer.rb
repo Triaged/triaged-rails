ebd6bc3833935d70bf5e2f28c2b8db49e18a291e
Charlie White <charlie@triaged.co>
2014-03-26 12:09:30 -0700

    datetimes

--- app/serializers/messages/chat_serializer.rb
+++ app/serializers/messages/chat_serializer.rb
@@ -1,9 +1,5 @@
 class Messages::ChatSerializer < ApplicationSerializer
   attributes :id, :body, :timestamp, :uuid, :author_name, :created_at
-
   has_one :author
 
-  def author_name
-  	object.author.name
-  end
 end
