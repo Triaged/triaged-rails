9cdf9334f5cc966a9a72e3d64ba5329ca63b4cbd
Charlie White <charlie@triaged.co>
2014-03-26 11:54:43 -0700

    datetimes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140326182324) do
+ActiveRecord::Schema.define(version: 20140326185050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,8 @@ ActiveRecord::Schema.define(version: 20140326182324) do
     t.string   "body"
     t.string   "user_mentions", default: [], array: true
     t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "notifications", force: true do |t|
