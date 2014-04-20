f432615674015f3586bbadb65b47d3388c6f8b53
Charlie White <charlie@triaged.co>
2014-04-20 19:19:51 -0400

    assets precompile

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -50,7 +50,7 @@ TriageRails::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w(welcome.css)
+  config.assets.precompile += %w(welcome.css home.css scrollReveal.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
