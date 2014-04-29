42876545aba83ed5dfcd4f9729193860760dd226
Charlie White <charlie@triaged.co>
2014-04-29 15:50:34 -0700

    updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Provider.create(name: "stripe", webhooks_enabled: false, title: "Stripe", short_title: "Stripe", oauth: true, active: true) # This is odd, because stripe does use a webhook, but because of setup, we turn off.
-Provider.create(name: "github", webhooks_enabled: true, title: "GitHub", short_title: "GitHub", account_label: 'Organization', property_label: 'Repo', oauth: true, active: true)
+Provider.create(name: "stripe", webhooks_enabled: false, title: "Stripe", short_title: "Stripe", oauth: true, active: true, ouath_path: "stripe_connect") # This is odd, because stripe does use a webhook, but because of setup, we turn off.
+Provider.create(name: "github", webhooks_enabled: true, title: "GitHub", short_title: "GitHub", account_label: 'Organization', property_label: 'Repo', oauth: true, active: true, ouath_path: "github")
 Provider.create(name: "bitbucket", webhooks_enabled: true, title: "Bitbucket", short_title: "Bitbucket", account_label: 'Organization', property_label: 'Repo', oauth: true, active: true)
-Provider.create(name: "google_analytics", webhooks_enabled: false, title: "Google Analytics", short_title: "Google", account_label: 'Account', property_label: 'Property', oauth: true, active: true)
+Provider.create(name: "google_analytics", webhooks_enabled: false, title: "Google Analytics", short_title: "Google", account_label: 'Account', property_label: 'Property', oauth: true, active: true, ouath_path: "google_oauth2")
 Provider.create(name: "new_relic", webhooks_enabled: true, title: "New Relic", short_title: "New Relic", active: true)
 Provider.create(name: "heroku", webhooks_enabled: true, title: "Heroku", short_title: "Heroku", active: true)
 Provider.create(name: "sentry", webhooks_enabled: true, title: "Sentry", short_title: "Sentry", active: true)
@@ -20,7 +20,7 @@ Provider.create(name: "hockey_app", webhooks_enabled: true, title: "HockeyApp", 
 Provider.create(name: "crashlytics", webhooks_enabled: true, title: "Crashlytics", short_title: "Crashlytics", active: true)
 Provider.create(name: "braintree", webhooks_enabled: true, title: "Braintree", short_title: "Braintree", active: true)
 Provider.create(name: "triage", webhooks_enabled: false, title: "Triage", short_title: "Triage", active: true)
-Provider.create(name: "dropbox", webhooks_enabled: false, title: "Dropbox", short_title: "Dropbox", oauth: true, active: true)
-Provider.create(name: "trello", webhooks_enabled: false, title: "Trello", short_title: "Trello", oauth: true, active: true)
-Provider.create(name: "appfigures", webhooks_enabled: false, title: "appFigures", short_title: "appFigures", account_label: 'Account', property_label: 'App', oauth: true, active: true)
+Provider.create(name: "dropbox", webhooks_enabled: false, title: "Dropbox", short_title: "Dropbox", oauth: true, active: true, ouath_path: "dropbox_oauth2")
+Provider.create(name: "trello", webhooks_enabled: false, title: "Trello", short_title: "Trello", oauth: true, active: true, ouath_path: "trello")
+Provider.create(name: "appfigures", webhooks_enabled: false, title: "appFigures", short_title: "appFigures", account_label: 'Account', property_label: 'App', oauth: true, active: true, ouath_path: "appFigures")
 Provider.create(name: "zapier", webhooks_enabled: true, title: "Zapier", short_title: "Zapier", active: true)
