b30e32582e26b5fff74191abdfb3a886a0d9f25b
Charlie White <charlie@triaged.co>
2014-05-05 18:06:28 -0700

    app follows

--- app/serializers/company_app_serializer.rb
+++ app/serializers/company_app_serializer.rb
@@ -1,3 +1,8 @@
 class CompanyAppSerializer < ApplicationSerializer
-  attributes :id, :name, :app_type
+  attributes :id, :name, :app_type, :follows
+
+  def follows
+  	!current_user.ignores? object
+  end
+
 end
